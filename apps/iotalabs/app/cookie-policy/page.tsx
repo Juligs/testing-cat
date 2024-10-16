2857a95c03152e2e1e0d558f8df4d3ce9680ddae
Juliana <115430927+Juligs@users.noreply.github.com>
2024-10-16 15:04:54 +0200

    fix(iotalabs): navbar theme not changing in cookie policy (#108)
    * fix:navbar theme scrolling
    
    * fix: update CookiePolicy component
    
    * fix: delete empty tag wrapper

--- apps/iotalabs/app/cookie-policy/page.tsx
+++ apps/iotalabs/app/cookie-policy/page.tsx
@@ -1,20 +1,14 @@
 'use client';
 
 import { CookieLibrary } from '@boxfish-studio/react-cookie-manager';
-
 import './CookiePolicy.scss';
-import { MdxPageHero } from 'app/(mdx-pages)/components';
+import { MdxPage } from 'app/(mdx-pages)/components';
 
 export default function CookiePolicy() {
     const configuration = {};
     return (
-        <>
-            <MdxPageHero />
-            <section className="container flex flex-col w-full justify-center items-center py-30">
-                <div className="prose prose-labs max-w-full">
-                    <CookieLibrary configuration={configuration} />
-                </div>
-            </section>
-        </>
+        <MdxPage>
+            <CookieLibrary configuration={configuration} />
+        </MdxPage>
     );
 }
