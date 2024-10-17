a785ebe1930fcf15701ce15885e66f2a230f5a51
Juliana <115430927+Juligs@users.noreply.github.com>
2024-10-17 11:22:10 +0200

    fix/update layout issue at breakpoint (#110)

--- apps/iotalabs/app/sections/homepage/FifthSection.tsx
+++ apps/iotalabs/app/sections/homepage/FifthSection.tsx
@@ -20,7 +20,7 @@ export function FifthSection({ id }: { id: string }) {
     return (
         <section className="w-full bg-labs-neutral-100" id={id}>
             <div className="container py-14 xs:py-20 lg:py-30 flex flex-col xs:flex-row justify-between text-center gap-14 xs:items-baseline">
-                <div className="sm:w-1/2 xl:w-2/5 shrink-0">
+                <div className="xs:w-1/2 xl:w-2/5 shrink-0">
                     <VerticalTitle
                         size={VerticalTitleTextSize.Medium}
                         title="Getting Started With IOTA"
