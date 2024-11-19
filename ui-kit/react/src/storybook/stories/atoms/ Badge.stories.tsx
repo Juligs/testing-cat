fc767bac5cb53d686df53432467e0e00c59c402b
Juliana <115430927+Juligs@users.noreply.github.com>
2024-11-19 13:34:24 +0100

    feat(ui-kit): create badge component (#169)
    * feat:create badge component (wip)
    
    * feat: create badge component(wip)
    
    * feat: add text color
    
    * fix: delete unnecessary class
    
    * refactor: update badge text colors
    
    * refactor: conditional ternary

---
+++ ui-kit/react/src/storybook/stories/atoms/ Badge.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Badge as BadgeComponent } from '@lib/server-components/atoms';
+const meta: Meta<typeof BadgeComponent> = {
+    component: BadgeComponent,
+    title: 'Atoms/Badge',
+    argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof BadgeComponent>;
+
+export const Badge: Story = {
+    render: (args) => <BadgeComponent {...args} />,
+};
