9faebb680388b767799d13e9d1fc26e3b97f6906
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2025-02-27 16:36:15 +0100

    fix(ui-kit): add more fallback fonts for more browser compatibilities (#515)
    * fix(ui-kit): add more fallback fonts for more browser compatibilities
    
    * chore: rename helvetica

--- ui-kit/react/src/lib/tailwind/constants/fontFamilies.constants.ts
+++ ui-kit/react/src/lib/tailwind/constants/fontFamilies.constants.ts
@@ -1,5 +1,19 @@
-export const FONT_FAMILIES: Record<string, [string, string]> = {
-    inter: ['Inter Variable', 'sans-serif'],
-    'alliance-no2': ['AllianceNo2', 'sans-serif'],
-    'red-hat-display': ['Red Hat Display Variable', 'sans-serif'],
+export const FALLBACK_FONTS = [
+    'system-ui',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+];
+
+export const FONT_FAMILIES: Record<string, string[]> = {
+    inter: ['Inter Variable', ...FALLBACK_FONTS],
+    'alliance-no2': ['AllianceNo2', ...FALLBACK_FONTS],
+    'red-hat-display': ['Red Hat Display Variable', ...FALLBACK_FONTS],
 } as const;
