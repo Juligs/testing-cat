0791764402671e0d4bbb3e5063dd0ad3d1b350e7
Juligs <julicodes29@gmail.com>
2025-03-06 15:32:29 +0100

    feat(iota-foundation): add research logos to shared file

--- apps/iota-foundation/app/sections/research/research-collaborations/constants/researchPartnersContent.constants.ts
+++ apps/iota-foundation/app/sections/research/research-collaborations/constants/researchPartnersContent.constants.ts
@@ -1,8 +1,8 @@
 export const RESEARCH_PARTNERS_LOGOS = [
-    '/research/logos/circular.png',
-    '/research/logos/UCD.png',
-    '/research/logos/univerisity_oslo.png',
-    '/research/logos/imperial.png',
-    '/research/logos/CDL_BOT.png',
-    '/research/logos/university_zurich.png',
+    '/shared/shared-logos/business/circular.png',
+    '/shared/shared-logos/business/UCD.png',
+    '/shared/shared-logos/business/univerisity_oslo.png',
+    '/shared/shared-logos/business/imperial.png',
+    '/shared/shared-logos/business/CDL_BOT.png',
+    '/shared/shared-logos/business/university_zurich.png',
 ];
