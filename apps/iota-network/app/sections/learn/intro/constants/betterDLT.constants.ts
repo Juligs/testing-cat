2845db58a5fc59099d9e331c17cdfc78f415b335
evavirseda <evirseda@boxfish.studio>
2025-05-20 13:53:22 +0200

    remove ? (#671)

--- apps/iota-network/app/sections/learn/intro/constants/betterDLT.constants.ts
+++ apps/iota-network/app/sections/learn/intro/constants/betterDLT.constants.ts
@@ -1,7 +1,7 @@
 import { TitleTextSize } from 'react-ui-kit';
 
 export const BETTER_DLT_VERTICAL_TITLE_CONTENT = {
-    title: 'Building a Better DLT?',
+    title: 'Building a Better DLT',
     subtitle: 'The next generation of blockchain',
     body: 'IOTA fundamentally differs from traditional blockchains. Instead of relying on linear blocks processed at fixed intervals, IOTA uses a unique data structure that enables interaction at any point in time, allowing operations to occur in parallel.',
     size: TitleTextSize.Small,
