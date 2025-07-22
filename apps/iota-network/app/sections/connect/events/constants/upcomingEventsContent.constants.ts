f8c7a04ce4a5cfcfe298e8762a5083de96df248d
evavirseda <evirseda@boxfish.studio>
2025-07-22 10:16:08 +0200

    Upcoming Events instead of Upcoming Event (#737)

--- apps/iota-network/app/sections/connect/events/constants/upcomingEventsContent.constants.ts
+++ apps/iota-network/app/sections/connect/events/constants/upcomingEventsContent.constants.ts
@@ -1,7 +1,7 @@
 import { TitleTextSize } from 'react-ui-kit';
 
 export const UPCOMING_EVENTS_VERTICAL_TITLE = {
-    title: 'Upcoming Event',
+    title: 'Upcoming Events',
     subtitle:
         'Find the latest dates and information about conferences, summits, and meet-ups near you.',
     size: TitleTextSize.Small,
