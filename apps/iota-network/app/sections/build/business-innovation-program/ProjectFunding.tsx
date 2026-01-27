004b265ecccbb3a4ae5b625e784f7572d2bc9179
Juliana <115430927+Juligs@users.noreply.github.com>
2026-01-27 15:00:51 +0100

    feat(iota-network): update project funding section (#868)

--- apps/iota-network/app/sections/build/business-innovation-program/ProjectFunding.tsx
+++ apps/iota-network/app/sections/build/business-innovation-program/ProjectFunding.tsx
@@ -1,5 +1,5 @@
 import { BaseSectionProps } from '@repo/shared/interfaces';
-import { VerticalTitle, Actions, DisplayStats, Button, ButtonVariant } from 'react-ui-kit';
+import { VerticalTitle, Actions, DisplayStats, Button, TextSize } from 'react-ui-kit';
 import { PROJECT_FUNDING_VERTICAL_TITLE, PROJECT_FUNDING_STATS_CONTENT } from './constants';
 import Link from 'next/link';
 import { TwoColumnsImageTemplate } from '@repo/shared/components';
@@ -14,18 +14,11 @@ export function ProjectFunding({ id, navbarColorScheme }: BaseSectionProps) {
                         <VerticalTitle {...PROJECT_FUNDING_VERTICAL_TITLE}>
                             <Actions>
                                 <Link
-                                    href="https://airtable.com/appoUvqpIo5bkDoSO/pag16jQChVVMh3zyh/form?ref=blog.iota.org"
+                                    href="https://blog.iota.org/iota-business-innovation-program/"
                                     target="_blank"
                                     rel="nopener noreferrer"
                                 >
-                                    <Button text="Apply now" icon={<ArrowTopRight />} />
-                                </Link>
-                                <Link
-                                    href="https://blog.iota.org/iota-business-innovation-program"
-                                    target="_blank"
-                                    rel="nopener noreferrer"
-                                >
-                                    <Button text="Blog post" variant={ButtonVariant.Secondary} />
+                                    <Button text="Blog post" icon={<ArrowTopRight />} />
                                 </Link>
                             </Actions>
                         </VerticalTitle>
@@ -33,7 +26,7 @@ export function ProjectFunding({ id, navbarColorScheme }: BaseSectionProps) {
                 </TwoColumnsImageTemplate>
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
                     {PROJECT_FUNDING_STATS_CONTENT.map((data, index) => (
-                        <DisplayStats key={index} {...data} />
+                        <DisplayStats key={index} {...data} textSize={TextSize.Small} />
                     ))}
                 </div>
             </div>
--- apps/iota-network/app/sections/build/business-innovation-program/constants/projectFundingContent.constants.ts
+++ apps/iota-network/app/sections/build/business-innovation-program/constants/projectFundingContent.constants.ts
@@ -2,23 +2,23 @@ import { TitleTextSize } from 'react-ui-kit';
 
 export const PROJECT_FUNDING_VERTICAL_TITLE = {
     title: 'Fuelling Innovation',
-    subtitle: 'Funding breakthroughs on the IOTA Mainnet',
-    body: "The IOTA Business Innovation Program offers up to €100,000 per applicant along with technical and strategic support to help businesses build real-world solutions on IOTA's scalable Mainnet.",
+    subtitle: 'Funding innovation on the IOTA Mainnet',
+    body: "The IOTA Business Innovation Program offers technical and strategic support to applicants to help businesses build real-world solutions on IOTA's scalable Mainnet.",
     size: TitleTextSize.Medium,
 };
 
 export const PROJECT_FUNDING_STATS_CONTENT = [
     {
-        label: 'Funding up to',
-        value: '€100,000',
+        label: 'Projects funded in 2025',
+        value: '7 successfully funded projects',
         highlighted: true,
     },
     {
-        label: 'Submission deadline',
-        value: '31 December 2025',
+        label: 'Application process',
+        value: 'Temporarily pausing while we restructure the Program for 2026',
     },
     {
-        label: 'Build requirement',
-        value: 'IOTA Mainnet',
+        label: 'Updates coming soon',
+        value: 'Stay tuned',
     },
 ];
