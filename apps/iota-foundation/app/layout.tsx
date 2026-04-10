e5a655a892f00e2638e226a1815a0ff2d2f45372
Juliana <115430927+Juligs@users.noreply.github.com>
2026-04-10 16:49:21 +0200

    Update layout.tsx

--- apps/iota-foundation/app/layout.tsx
+++ apps/iota-foundation/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                     <Footer />
                     <CookieDisclaimer />
                 </ContextProviders>
-            </body>
+            ;</body>
         </html>
     );
 }
